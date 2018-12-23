0424b00b5eda9ae3e769ed3d2346bb63b65bd156
Aaron Cimolini <aaron@cimolini.com>
2018-12-23 15:28:24 -0800

    Starts work styling the homepage hero.

--- web/app/themes/staydry-storefront/resources/templates/partials/homepage-hero.tpl.php
+++ web/app/themes/staydry-storefront/resources/templates/partials/homepage-hero.tpl.php
@@ -1,12 +1,14 @@
 <div class="homepage-hero">
-    <h2><?= $hero['headline'] ?></h2>
-    <h3><?= $hero['sub-heading'] ?></h3>
-    <ul>
-        <?php foreach ($hero['feature_bullets'] as $feature): ?>
-            <li><?= $feature['text'] ?></li>
-        <?php endforeach; ?>
-    </ul>
     <div class="homepage-hero__image">
         <img src="<?php echo $hero['image']['url']; ?>" alt="<?php echo $hero['image']['alt']; ?>" />
+    </div>
+    <div class="homepage-hero__content">
+        <h2 class=""><?= $hero['headline'] ?></h2>
+        <h3><?= $hero['sub-heading'] ?></h3>
+        <ul>
+            <?php foreach ($hero['feature_bullets'] as $feature): ?>
+                <li><?= $feature['text'] ?></li>
+            <?php endforeach; ?>
+        </ul>
     </div>
 </div>
