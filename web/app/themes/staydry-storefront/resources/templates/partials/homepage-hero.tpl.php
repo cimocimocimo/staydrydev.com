062026f1fde0c56d1041a47df82e1a0ba7ea4705
Aaron Cimolini <aaron@cimolini.com>
2018-12-25 00:14:55 -0800

    Adds responsive styling for the homepage hero.

--- web/app/themes/staydry-storefront/resources/templates/partials/homepage-hero.tpl.php
+++ web/app/themes/staydry-storefront/resources/templates/partials/homepage-hero.tpl.php
@@ -1,9 +1,14 @@
 <div class="homepage-hero">
+    <div class="homepage-hero__headline-desktop">
+        <h2><?= $hero['headline'] ?></h2>
+    </div>
     <div class="homepage-hero__image">
         <img src="<?php echo $hero['image']['url']; ?>" alt="<?php echo $hero['image']['alt']; ?>" />
     </div>
     <div class="homepage-hero__content">
-        <h2 class=""><?= $hero['headline'] ?></h2>
+        <div class="homepage-hero__headline-handheld">
+            <h2><?= $hero['headline'] ?></h2>
+        </div>
         <h3><?= $hero['sub-heading'] ?></h3>
         <ul>
             <?php foreach ($hero['feature_bullets'] as $feature): ?>
