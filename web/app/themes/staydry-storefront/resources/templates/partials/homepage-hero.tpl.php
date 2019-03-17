04bbab8f4b25b1c3a371f2929c4222c3874c5bee
Aaron Cimolini <aaron@cimolini.com>
2019-03-17 16:54:07 -0700

    Styling of the homepage hero.

--- web/app/themes/staydry-storefront/resources/templates/partials/homepage-hero.tpl.php
+++ web/app/themes/staydry-storefront/resources/templates/partials/homepage-hero.tpl.php
@@ -1,54 +1,31 @@
 <?php
 
-    use function Tonik\Theme\App\asset_path;
+    use function Tonik\Theme\App\template;
 
 ?>
 <div class="homepage-hero">
     <div class="homepage-hero__headline-desktop">
         <h2><?= $headline ?></h2>
     </div>
-    <div class="homepage-hero__problem">
-        <div class="homepage-hero__problem-heading">
-            <h3>The<br>Problems</h3>
-        </div>
-        <div class="homepage-hero__problem-image">
-            <img src="<?= $problem_image['sizes']['large'] ?>" alt="Problem image" />
-        </div>
-        <div class="homepage-hero__problem-features">
-            <?php foreach ($problem_features as $feature): ?>
-                <div class="solution-feature solution-feature--icon-<?= $feature['icon_position'] ?>"
-                    style="
-                    left: <?= $feature['horizontal_position'] ?>%;
-                    top: <?= $feature['vertical_position'] ?>%;">
-                    <span class="outline"><?= $feature['text'] ?></span>
-                    <span class="text"><?= $feature['text'] ?></span>
-                    <img src="<?= asset_path('images/red-x.svg') ?>"
-                        alt="Red X Icon">
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </div>
-    <div class="homepage-hero__solution">
-        <div class="homepage-hero__solution-heading">
-            <h3>Our<br>Solution</h3>
-        </div>
-        <div class="homepage-hero__solution-image">
-            <img src="<?= $solution_image['sizes']['large'] ?>" alt="Solution image" />
-        </div>
-        <div class="homepage-hero__solution-features">
-            <?php foreach ($solution_features as $feature): ?>
-                <div class="solution-feature solution-feature--icon-<?= $feature['icon_position'] ?>"
-                    style="
-                    left: <?= $feature['horizontal_position'] ?>%;
-                    top: <?= $feature['vertical_position'] ?>%;">
-                    <span class="outline"><?= $feature['text'] ?></span>
-                    <span class="text"><?= $feature['text'] ?></span>
-                    <img src="<?= asset_path('images/green-check.svg') ?>"
-                        alt="Green Check Icon">
-                </div>
-            <?php endforeach; ?>
-        </div>
-    </div>
+
+    <?= template('partials/homepage-hero-panel', [
+        'class' => 'homepage-hero__problem',
+            'heading' => 'The<br>Problems',
+            'image' => $problem_image,
+            'features' => $problem_features,
+            'icon_path' => 'images/red-x.svg',
+            'feature_class' => 'problem',
+        ]) ?>
+
+    <?= template('partials/homepage-hero-panel', [
+        'class' => 'homepage-hero__solution',
+            'heading' => 'Our<br>Solution',
+            'image' => $solution_image,
+            'features' => $solution_features,
+            'icon_path' => 'images/green-check.svg',
+            'feature_class' => 'solution',
+        ]) ?>
+
     <div class="homepage-hero__headline-handheld">
         <h2><?= $headline ?></h2>
     </div>
