1fbfa0b97ec7a72a63183a05c5427bd767fd7833
Aaron Cimolini <aaron@cimolini.com>
2019-05-05 17:01:17 -0700

    Adds desktop animation timeline.

--- web/app/themes/staydry-storefront/resources/assets/js/routes/home.js
+++ web/app/themes/staydry-storefront/resources/assets/js/routes/home.js
@@ -6,49 +6,60 @@ import TweenMax from 'gsap/TweenMax'
 import ScrollMagic from 'scrollmagic/scrollmagic/uncompressed/ScrollMagic'
 import ScrollToPlugin from 'gsap/ScrollToPlugin'
 
-function createFeatureTimeline ($features, mainTimeline) {
-  var timeline = new TimelineMax({paused: true});
+var heroAnimation = {
+  timelines: {
+    mobile: {
+      main: null,
+      features: {
+        problems: null,
+        solutions: null,
+      }
+    },
+    desktop: {
+      main: null,
+      features: {
+        problems: null,
+        solutions: null,
+      }
+    },
+  },
 
-  timeline.add(function () {
-    mainTimeline.pause();
-  }).staggerFromTo(
-    $features, 1,
-    {scale: .5, y: '-=40px',autoAlpha: 0},
-    {scale: 1, y: 0, autoAlpha: 1, ease:Back.easeOut},
-    0.25
-  ).add(function () {
-    mainTimeline.play();
-  });
+  createFeatureTimeline ($features, mainTimeline) {
+    var timeline = new TimelineMax({paused: true});
 
-  return function () {
-    timeline.play();
-  }
-}
+    timeline.add(function () {
+      mainTimeline.pause();
+    }).staggerFromTo(
+      $features, 1,
+      {scale: .5, y: '-=40px',autoAlpha: 0},
+      {scale: 1, y: 0, autoAlpha: 1, ease:Back.easeOut},
+      0.25
+    ).add(function () {
+      mainTimeline.play();
+    }).pause();
 
-function createTimeline () {
+    return timeline;
+  },
 
-}
+  wirePanelClickHandlers ($problem, $solution, timeline) {
+    $problem.click(function () {
+      timeline.tweenTo('swapStart');
+    });
+    $solution.click(function () {
+      timeline.tweenTo('swapEnd');
+    });
+  },
 
-function wirePanelClickHandlers ($problem, $solution, timeline) {
-  $problem.click(function () {
-    timeline.tweenTo('swapStart');
-  });
-  $solution.click(function () {
-    timeline.tweenTo('swapEnd');
-  });
-}
-
-export default {
-  init() {
-
-    var $hero = $('.homepage-hero'),
-        $panels = $('.panel__problem, .panel__solution'),
-        $problem = $('.panel__problem'),
-        $solution = $('.panel__solution'),
-        $features = $hero.find('.problem-feature, .solution-feature'),
-        $problemFeatures = $hero.find('.problem-feature'),
-        $solutionFeatures = $hero.find('.solution-feature'),
+  initTimeline ($stage, isDesktop = false) {
+    var $panels = $stage.find('.panel__problem, .panel__solution'),
+        $problem = $stage.find('.panel__problem'),
+        $solution = $stage.find('.panel__solution'),
+        $problemFeatures = $stage.find('.problem-feature'),
+        $solutionFeatures = $stage.find('.solution-feature'),
         timeline = new TimelineMax({paused: true});
+
+    var problemFeaturesTimeline = this.createFeatureTimeline($problemFeatures, timeline);
+    var solutionFeaturesTimeline = this.createFeatureTimeline($solutionFeatures, timeline);
 
     timeline
       .from(
@@ -57,19 +68,61 @@ export default {
         {
           scale: 0.5, autoAlpha: 0
         })
-      .add(createFeatureTimeline($problemFeatures, timeline))
-      .add('swapStart')
-      .to($problem, 1.5, {scale: 0.5, left: '-5%', ease: Circ.easeInOut}, 'swapStart')
-      .to($solution, 1.5, {scale: 0.5, right: '-5%', ease: Circ.easeInOut}, 'swapStart')
-      .add('swapMid')
-      .set($problem, {zIndex: 0})
-      .set($solution, {zIndex: 10})
-      .to($problem, 1.5, {scale: 0.7, left: '0%', ease: Circ.easeInOut}, 'swapMid')
-      .to($solution, 1.5, {scale: 0.9, right: '0%', ease: Circ.easeInOut}, 'swapMid')
-      .add(createFeatureTimeline($solutionFeatures, timeline))
-      .add('swapEnd')
-      .call(wirePanelClickHandlers, [$problem, $solution, timeline]);
+      .add(function () {
+        problemFeaturesTimeline.play()
+      });
 
+
+    if (isDesktop) {
+      timeline
+        .add('swapStart')
+        .to($problem, 1.5, {scale: 0.7, left: '0%', ease: Circ.easeInOut})
+        .set($problem, {zIndex: 0})
+        .set($solution, {zIndex: 10})
+        .to($solution, 1.5, {scale: 0.9, right: '0%', ease: Circ.easeInOut})
+        .add(function () {
+          solutionFeaturesTimeline.play()
+        })
+        .add('swapEnd');
+    } else {
+      timeline
+        .add('swapStart')
+        .to($problem, 1.5, {scale: 0.5, left: '-5%', ease: Circ.easeInOut}, 'swapStart')
+        .to($solution, 1.5, {scale: 0.5, right: '-5%', ease: Circ.easeInOut}, 'swapStart')
+        .add('swapMid')
+        .set($problem, {zIndex: 0})
+        .set($solution, {zIndex: 10})
+        .to($problem, 1.5, {scale: 0.7, left: '0%', ease: Circ.easeInOut}, 'swapMid')
+        .to($solution, 1.5, {scale: 0.9, right: '0%', ease: Circ.easeInOut}, 'swapMid')
+        .add(function () {
+          solutionFeaturesTimeline.play()
+        })
+        .add('swapEnd')
+        .call(this.wirePanelClickHandlers, [$problem, $solution, timeline]);
+    }
+
+    return {
+      main: timeline,
+      features: {
+        problems: problemFeaturesTimeline,
+        solutions: solutionFeaturesTimeline
+      }
+    };
+  },
+
+  init () {
+    var self = this,
+        $hero = $('.homepage-hero'),
+        $mobileStage = $('.homepage-hero__stage-mobile'),
+        $desktopStage = $('.homepage-hero__stage-desktop');
+
+    this.timelines.mobile = this.initTimeline($mobileStage);
+    this.timelines.desktop = this.initTimeline($desktopStage, true);
+
+    $(window).load(function(){
+      self.timelines.mobile.main.play()
+      self.timelines.desktop.main.play()
+    });
 
     // TODO: remove in production
     var $controls = $('.animation-controls'),
@@ -79,27 +132,42 @@ export default {
         $reverse = $('#animation-reverse'),
         $restart = $('#animation-restart');
     $play.on('click', function(){
-      timeline.play();
+      self.timelines.mobile.main.play();
+      self.timelines.desktop.main.play();
       $(this).hide();
       $pause.show();
     });
     $pause.hide();
     $pause.on('click', function(){
-      timeline.pause();
+      self.timelines.mobile.main.pause();
+      self.timelines.desktop.main.pause();
       $(this).hide();
       $play.show();
     });
-    $resume.hide()
-    $resume.on('click', function(){
-      timeline.resume();
-    });
-    $reverse.on('click', function(){
-      timeline.reverse();
-    });
+    $reverse.hide();
+    $resume.hide();
     $restart.on('click', function(){
-      timeline.restart();
+      self.timelines.desktop.features.problems.seek(0).pause();
+      self.timelines.desktop.features.solutions.seek(0).pause();
+      self.timelines.mobile.features.problems.seek(0).pause();
+      self.timelines.mobile.features.solutions.seek(0).pause();
+      self.timelines.desktop.main.restart();
+      self.timelines.mobile.main.restart();
     });
+  },
 
+  play () {
+
+  },
+  reset () {
+    console.log('reset');
+  }
+};
+
+export default {
+  init() {
+
+    heroAnimation.init();
 
     // init controller
     var controller = new ScrollMagic.Controller();
